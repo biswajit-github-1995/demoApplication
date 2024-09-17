3930aebaab31b62bdcfe2402419b562a1f9fc92c
biswajit-github-1995 <kumarbiswajit.github@gmail.com>
2024-09-17 21:24:37 +0530

    project setup code checkin

--- src/test/java/com/learning/DemoApplicationTests.java
+++ src/test/java/com/learning/DemoApplicationTests.java
@@ -1,13 +1,8 @@
-package com.learning.demoApplication;
+package com.learning;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
